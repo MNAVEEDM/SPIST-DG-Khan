40f34112e363c8ec00e09c0a776d793b3a014033
Mian Ashar <mianashar05@gmail.com>
2026-09-01 15:42:39 +0500

    updated icon

--- src/data/site.js
+++ src/data/site.js
@@ -793,13 +793,14 @@ export const galleryItems = [
     id: 1,
     category: 'Labs',
     caption: 'Chemistry Research Laboratory',
-    src: '/campus gallary/lab/Chemistry Research Laboratory.jpeg',
+    src: 'public/campus gallary/lab/chem lab.jpg',
+    
   },
   {
     id: 2,
     category: 'Campus',
     caption: 'Main Academic Block',
-    src: '/campus gallary/campus/Main Academic Block.jpeg',
+    src: 'public/campus gallary/lab/campus.jpg',
   },
   {
     id: 3,
@@ -829,13 +830,13 @@ export const galleryItems = [
     id: 7,
     category: 'Campus',
     caption: 'Student Lounge & Cafeteria',
-    src: '/campus gallary/campus/Student Lounge & Cafeteria.jpeg',
+    src: 'public/campus gallary/cafeteria.jpg',
   },
   {
     id: 8,
     category: 'Labs',
     caption: 'Analytical Instrumentation Suite',
-    src: '/campus gallary/lab/Analytical Instrumentation Suite.jpeg',
+    src: 'public/campus gallary/lab/labs.jpg',
   },
 ];
 
